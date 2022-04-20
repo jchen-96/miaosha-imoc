f1f205784f386a2eb69f2322052e7329ebad966f
jiancheng <17628293212@165.com>
2022-04-21 00:27:44 +0800

    init

---
+++ common/datasourse/redis.go
@@ -0,0 +1,34 @@
+package datasourse
+
+import (
+	"github.com/kataras/iris/sessions/sessiondb/redis"
+	"github.com/kataras/iris/sessions/sessiondb/redis/service"
+	"jiancheng.com/imooc/miaosha/common/conf"
+)
+
+var redisInstance *redis.Database
+
+func GetRedisInstance() *redis.Database {
+	if redisInstance == nil {
+		redisInstance = initRedis()
+	}
+	return redisInstance
+}
+
+func initRedis() *redis.Database {
+	var dataBase *redis.Database
+
+	rd := conf.RedisSetting
+	dataBase = redis.New(service.Config{
+		Network:     rd.NetWork,
+		Addr:        rd.Host,
+		Password:    rd.Password,
+		Database:    "",
+		MaxIdle:     rd.MaxIdle,
+		MaxActive:   rd.MaxActive,
+		IdleTimeout: service.DefaultRedisIdleTimeout,
+		Prefix:      rd.Prefix,
+	})
+
+	return dataBase
+}
---
+++ datamodels/Message.go
@@ -0,0 +1,33 @@
+package datamodels
+
+import (
+	"encoding/json"
+
+	"jiancheng.com/imooc/miaosha/common/conf"
+)
+
+// 商品下单用于消息队列传输的消息
+type Message struct {
+	ProductID int64
+	UserID    int64
+}
+
+func NewMessage(productID int64, userID int64) *Message {
+	return &Message{ProductID: productID, UserID: userID}
+}
+
+func (m *Message) JsonToStr() string {
+	bytes, err := json.Marshal(m)
+	if err != nil {
+		conf.AppSetting.Logger.Error("json 解析出错：" + err.Error())
+	}
+
+	return string(bytes)
+}
+
+func (m *Message) StrToJson(dataStr []byte) *Message {
+	if err := json.Unmarshal(dataStr, m); err != nil {
+		conf.AppSetting.Logger.Error("json 转换出错：" + err.Error())
+	}
+	return m
+}
